efbd14cae6dc5024686bf7d3a8d3b55383975855
ronak1603 <98402931+ronak1603@users.noreply.github.com>
2022-08-10 11:51:29 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import Task from "./Components/Task";
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <Task/>
+    </div>
+  );
+}
+
+export default App;
